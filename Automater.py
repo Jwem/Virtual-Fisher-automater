5481d0124eec351f64cfb67653bfc2028ea59ef3
Jwem <boazcohenj@gmail.com>
2025-02-09 22:14:54 +0200

    Added balance check and remote turn off by tyipng "Remote" in the chat

--- Automater.py
+++ Automater.py
@@ -8,12 +8,17 @@ import threading
 # Specify the path to the Tesseract executable if it's not in your PATH
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-# Initialize delay as a global variable
-delay = 3.2
+# Initialize global variables
+delay = 2.7
 antiBotActive = False
 buying = False
 boat = False
 stop_script = False
+last_command_time = time.time()
+last_click_time = time.time()
+last_boost_time = time.time()
+last_upgrade_time = time.time()
+last_daily_time = time.time()
 
 def check_pixel_color_private_msg():
     x, y = 1500, 880
@@ -35,16 +40,29 @@ def image_captcha():
 
 def upgrade_boat():
     global boat  # add this line to use the global variable
+    global last_click_time
     boat = True
     coordinates = [(642, 925), (500, 915), (650, 883), (500, 925)]
     for x, y in coordinates:
         pyautogui.click(x, y)
         time.sleep(2)
+    last_click_time = time.time()
 
-def buying_func():
+def check_balance():
+    region = (310, 750, 374, 153)
+    screenshot = pyautogui.screenshot(region=region)
+    text = pytesseract.image_to_string(screenshot)
+    if "You now have $" in text:
+        balance = text.split("You now have")[1].split("!")[0]
+        return balance
+    else:
+        return "Balance not found"
+
+def buying_boosts():
     global buying
-    global current_time
+    global last_boost_time
     buying = True
+    print("Buying boosts...")
     commands = [
         '/buy Auto10m',
         '/buy Fish5m',
@@ -55,11 +73,41 @@ def buying_func():
         '/buy Fish Ovens',
     ]
     for command in commands:
+        if check_anti_bot_and_verify():
+            print("Anti-bot verification failed during buying boosts. Exiting...")
+            return
         pyautogui.write(command)
         pyautogui.press('enter')
         pyautogui.press('enter')
-        time.sleep(2)
-    last_command_time = current_time
+        time.sleep(3)
+    last_boost_time = time.time()
+
+def buying_upgrades():
+    global buying
+    global last_upgrade_time
+    buying = True
+    print("Buying upgrades...")
+    upgrades = [
+        '/buy Better Fish',
+        '/buy Salesman',
+        '/buy Bait Efficiency',
+        '/buy More Chests',
+        '/buy Worker Motivation',
+        '/buy Better Fish',
+        '/buy Artifact Specialist',
+        '/buy Experienced',
+        '/buy Better Chests',
+        '/buy Better Dailies',
+    ]
+    for upgrade in upgrades:
+        if check_anti_bot_and_verify():
+            print("Anti-bot verification failed during buying upgrades. Exiting...")
+            return
+        pyautogui.write(upgrade)
+        pyautogui.press('enter')
+        pyautogui.press('enter')
+        time.sleep(3)
+    last_upgrade_time = time.time()
 
 def remote_turn_off():
     text = text_captcha()
@@ -81,19 +129,28 @@ def check_anti_bot_and_verify():
             # Remove non-alphanumeric characters
             code = ''.join(e for e in code if e.isalnum())
             if code:
-                pyautogui.write(f"/verify {code}")
+                pyautogui.write(f"/verify")
+                time.sleep(0.2)  # Wait for the command to be typed
+                pyautogui.write(" " + code)
+                time.sleep(0.2)  # Wait for the code to be typed
                 pyautogui.press('enter')
                 result_text = text_captcha()
-                time.sleep(3)  # Wait for verification to complete
+                time.sleep(5)  # Wait for verification to complete
+
                 if "Incorrect" not in result_text:
                     print(f"Verified with code: {code}")
                     antiBotActive = False
                     return False  # Continue normal operation
+                
+                print(f"Verification failed with code: {code}")
+                print("Falling back to image captcha...")
 
         # Fallback to image captcha if "Code:" is absent
         img_text = image_captcha()
         img_text = ''.join(e for e in img_text if e.isalnum())
-        pyautogui.write(f"/verify {img_text}")
+        pyautogui.write(f"/verify")
+        time.sleep(0.2)  # Wait for the command to be typed
+        pyautogui.write(" " + img_text)
         pyautogui.press('enter')
         time.sleep(3)
         screenshot = pyautogui.screenshot(region=(407, 893, 140, 27))
@@ -107,6 +164,7 @@ def check_anti_bot_and_verify():
         regen_attempt = 0
         while regen_attempt < 5:
             pyautogui.write("/verify regen")
+            time.sleep(0.2)
             pyautogui.press('enter')
             print(f"Regeneration attempt {regen_attempt+1}")
             time.sleep(3)
@@ -119,7 +177,9 @@ def check_anti_bot_and_verify():
                 # Remove non-alphanumeric characters
                 code = ''.join(e for e in code if e.isalnum())
                 if code:
-                    pyautogui.write(f"/verify {code}")
+                    pyautogui.write(f"/verify")
+                    time.sleep(0.2)
+                    pyautogui.write(" " + code)
                     pyautogui.press('enter')
                     time.sleep(3)
                     screenshot = pyautogui.screenshot(region=(407, 893, 140, 27))
@@ -132,7 +192,9 @@ def check_anti_bot_and_verify():
                 # Fallback to image captcha if "Code:" is absent
                 img_text = image_captcha()
                 img_text = ''.join(e for e in img_text if e.isalnum())
-                pyautogui.write(f"/verify {img_text}")
+                pyautogui.write(f"/verify")
+                time.sleep(0.2)
+                pyautogui.write(" " + img_text)
                 pyautogui.press('enter')
                 time.sleep(3)
                 screenshot = pyautogui.screenshot(region=(407, 893, 140, 27))
@@ -154,7 +216,7 @@ def listen_for_esc():
     stop_script = True
 
 def main():
-    global delay, antiBotActive, buying, boat, stop_script
+    global delay, antiBotActive, buying, boat, stop_script, last_command_time, last_boost_time, last_upgrade_time, last_daily_time
     print("2 seconds to move to the desired window.")
     time.sleep(2)
 
@@ -188,27 +250,41 @@ def main():
                 time.sleep(5)
                 boat = False
 
-            if current_time - last_reduce_time >= 120:  # every 2 minutes
+            if current_time - last_reduce_time >= 120:  # Every 2 minutes
                 delay -= 0.1
                 print(f"Reduced delay: {delay}")
                 last_reduce_time = current_time
 
-            if current_time - last_click_time >= 3600:
+            if current_time - last_click_time >= 3600: # Every 1 hour
                 upgrade_boat()
                 last_click_time = current_time
             
-            if current_time - last_command_time >= 300:
-                buying_func()
+            if current_time - last_boost_time >= 300:# Every 5 minutes
+                buying_boosts()
+
+            if current_time - last_upgrade_time >= 1800: # Every 30 minutes
+                buying_upgrades()
+
+            if current_time - last_daily_time >= 86400: # Every 24 hours
+                pyautogui.write('/daily')
+                pyautogui.press('enter')
+                pyautogui.press('enter')
+                last_daily_time = current_time
 
             if check_pixel_color_private_msg():
                 print("Pixel color matched. Increasing delay...")
                 delay += 0.1
+                print(f"New delay: {delay}")
                             
-            if current_time - last_command_time >= 60:
+            if current_time - last_command_time >= 60: # Every 1 minute
                 pyautogui.write('/sell all')
                 pyautogui.press('enter')
                 pyautogui.press('enter')
+                time.sleep(1.5)
                 last_command_time = current_time
+                balance = check_balance()
+                print(f"Balance: {balance}")
+
             else:
                 pyautogui.write('/fish')
                 pyautogui.press('enter')
